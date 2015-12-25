01a737546610a3ca65736b9b2f5f1cbf35e7f015
afp <pierucci@gmail.com>
2015-12-25 23:20:31 +0100

    documented define_resample

--- R/resamp.R
+++ R/resamp.R
@@ -1,3 +1,16 @@
+#' Define parameters distribution for probabilistc analysis
+#' 
+#' Define the properties of parameter distributions and 
+#' their correlation structure for probabilistic incertitude
+#' analysis of Markov models.
+#' 
+#' @param ... Name-value pairs of expressions definig 
+#'   parameter distributions.
+#' @param mat_cor A correlation matrix for parameters.
+#'   
+#' @return An object of class \code{resamp_definition}.
+#' @export
+#' 
 define_resample <- function(...,
                             mat_cor = diag(length(list(...)))) {
   list_qdist <- list(...)
