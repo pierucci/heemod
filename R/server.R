7ce5f277f8e27dad84a5597cee005f54031f92fe
KZARCA <kevinounet@gmail.com>
2016-02-12 11:44:30 +0100

    More intuitive GUI Add errors handler file (empty for the moment)

--- R/server.R
+++ R/server.R
@@ -4,41 +4,43 @@ shinyServer(function(input, output) {
     counterState <- input$add - input$rem
     if (counterState < 0)
       counterState = 0
-    w <- NULL
-    nbNamedState = 0
-    if (counterState > 0){
-      for(i in 1:counterState) {
-        w <- paste(w, textInput(paste0("stateName", i), paste("State Name", i), value = input[[sprintf("stateName%d",i)]]))
-        nbNamedState <- ifelse(input[[sprintf("stateName%d",i)]] != "", nbNamedState + 1, nbNamedState)
-      }
+    if (counterState > 0){    
+      lapply(1:counterState, function(i) {
+        isolate({textInput(paste0("stateName", i), paste("State Name", i), value = ifelse(!is.null(input[[paste0("stateName",i)]]), input[[paste0("stateName",i)]], ""))})
+      })
     }
-    value$nbNamedStates <- nbNamedState
-    HTML(w)
   })
   
   output$transmatrix <- renderUI({
-    req(value$nbNamedStates)
-    nbNamedStates <- value$nbNamedStates
-    nbStates = input$nbStates
-    th <- ""
-    tr <- ""
-    h <- ""
-    tab <- ""
-    if (nbNamedStates > 0){
-      h <- h3("Transition Matrix")
-      tab <- "<table class='transmatrix'><th></th>"
-      for (i in 1:nbNamedStates){
-        th <- paste0(th, "<th style='text-align:center'>", input[[sprintf("stateName%d",i)]], "</th>")
-        tr <- paste0(tr, "<tr><td>",input[[sprintf("stateName%d",i)]],"</td>")
-        for (j in 1:nbNamedStates){
-          tr <- paste0(tr, "<td>", textInput(paste0("transmatrix",i,j), value="", label=NULL, width="100%"), "</td>")
+    counterState <- input$add - input$rem
+    req(counterState)
+    nbNamedStates <- 0
+    for (i in 1:counterState)
+      nbNamedStates <- ifelse(input[[paste0("stateName",i)]] != "", nbNamedStates + 1, nbNamedStates)
+    
+    req(nbNamedStates)
+    if  (nbNamedStates > 0)
+    {
+      th <- ""
+      tr <- ""
+      h <- ""
+      tab <- ""
+      if (nbNamedStates > 0){
+        h <- h3("Transition Matrix")
+        tab <- "<table class='transmatrix'><th></th>"
+        for (i in 1:nbNamedStates){
+          th <- paste0(th, "<th style='text-align:center'>", input[[paste0("stateName",i)]], "</th>")
+          tr <- paste0(tr, "<tr><td>",input[[paste0("stateName",i)]],"</td>")
+          for (j in 1:nbNamedStates){
+            tr <- paste0(tr, "<td>", textInput(paste0("transmatrix",i,j), value="", label=NULL, width="100%"), "</td>")
+          }
+          tr <- paste0(tr, "</tr>")
         }
-        tr <- paste0(tr, "</tr>")
+        tab <- paste0(tab, th, tr, "</table>")
       }
-      tab <- paste0(tab, th, tr, "</table>")
+      sortie <- tagList( h,
+        HTML(tab)
+      )
     }
-    sortie <- tagList( h,
-      HTML(tab)
-    )
-  })
+    })
 })
--- R/ui.R
+++ R/ui.R
@@ -1,16 +1,21 @@
 shinyUI(fluidPage(
   titlePanel("HEEMOD GUI"),
   tabsetPanel(
-      tabPanel("States",    
-               h3("Define States"),
+      tabPanel("States", h3("Define States"),
                actionButton("add","Add a State"),
                actionButton("rem","Remove Last State"),
-               uiOutput("nameStates"),
+               uiOutput("nameStates")
+      ),
+      tabPanel("Transition Matrix",    
                fluidRow(
                  column(12, uiOutput("transmatrix"))
                )
       ), 
-      tabPanel("Parameters",  "rien"
+      tabPanel("States Parameters",
+               numericInput("nbStrategies", label = "Number of Strategies", value="", min = 0),
+               uiOutput("nameStrategies")
+      ), 
+      tabPanel("Global Parameters",  "rien"
       ), 
       tabPanel("Output", tableOutput("out")
       )
