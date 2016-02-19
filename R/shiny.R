613ab1130b6b4f415ca0cbe8925bf4a86c56a344
afp <pierucci@gmail.com>
2016-02-19 15:20:08 +0100

    :wrench:changed shiny app filestructure

---
+++ R/shiny.R
@@ -0,0 +1,15 @@
+#' Run Shiny Graphical Interface
+#' 
+#' Run in browser the Shiny graphical interface to \code{heemod}.
+#'
+#' @return \code{NULL}
+#' @export
+#'
+run_shiny_main <- function() {
+    app_dir <- system.file("shiny", "main", package = "mypackage")
+    if (app_dir == "") {
+      stop("Could not find 'shiny' directory. Try re-installing `heemod`.", call. = FALSE)
+    }
+    
+    shiny::runApp(app_dir, display.mode = "normal")
+}
