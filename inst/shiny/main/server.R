f74ce5810bcafb7f818a131d90032c12cda1b4aa
afp <pierucci@gmail.com>
2016-02-25 17:12:40 +0100

    :tv:help & default ce

--- inst/shiny/main/server.R
+++ inst/shiny/main/server.R
@@ -18,7 +18,7 @@ shinyServer(function(input, output, session) {
       start <- ifelse(nbStrat>1, 2, 1)
       lapply(start:nbStrat, function(x){
         tagList(
-          h3(paste0("State Parameters for: \"", input[[paste0("strategyName",x)]], "\"")),
+          h3(paste("State Parameters for", input[[paste0("strategyName",x)]])),
           tags$table(class='stateVariables',
                      tagList(
                        tags$th(),
@@ -29,7 +29,7 @@ shinyServer(function(input, output, session) {
                        lapply(1:nbStateVariables, function(i){
                          tags$tr(tags$td(variableStateName[i]),
                                  lapply (1:nbStates, function (j) {
-                                   isolate({tags$td(numericInput(paste0("stateVariable",x,i,j), value=ifelse(!is.null(input[[paste0("stateVariable",1,i,j)]]), input[[paste0("stateVariable",1,i,j)]],0), label=NULL, width="100%"))})
+                                   isolate({tags$td(textInput(paste0("stateVariable",x,i,j), value=ifelse(!is.null(input[[paste0("stateVariable",1,i,j)]]), input[[paste0("stateVariable",1,i,j)]],0), label=NULL, width="100%"))})
                                  }),
                                  isolate({tags$td(numericInput(paste0("discountingRate",x,i), label=NULL, value=ifelse(!is.null(input[[paste0("discountingRate",1,i)]]), input[[paste0("discountingRate",1,i)]],0), width="100%"))})
                          )
@@ -53,7 +53,7 @@ shinyServer(function(input, output, session) {
       tagList(
         lapply(start:nbStrat, function(x){
           tagList(
-            h3(paste0("Transition Matrix for \"", input[[paste0("strategyName",x)]],"\"")),
+            h3(paste("Transition Matrix for", input[[paste0("strategyName",x)]])),
             tags$table(class='transmatrix',
                        tagList(
                          tags$th(),
@@ -80,9 +80,9 @@ shinyServer(function(input, output, session) {
   
   output$nameStates <- renderUI({
     req(input$nbStates)
-      lapply(1:input$nbStates, function(i) {
-        isolate({textInput(paste0("stateName", i), paste("State Name", i), value = ifelse(!is.null(input[[paste0("stateName",i)]]), input[[paste0("stateName",i)]], paste("State",LETTERS[i])))})
-      })
+    lapply(1:input$nbStates, function(i) {
+      isolate({textInput(paste0("stateName", i), paste("State Name", i), value = ifelse(!is.null(input[[paste0("stateName",i)]]), input[[paste0("stateName",i)]], LETTERS[i]))})
+    })
   })
   output$nameStateVariables <- renderUI({
     req(input$nbStateVariables)
@@ -93,13 +93,13 @@ shinyServer(function(input, output, session) {
   output$nameStrategies <- renderUI({
     req(input$nbStrategies)
     lapply(1:input$nbStrategies, function(i) {
-      isolate({textInput(paste0("strategyName", i), paste("Strategy Name", i), value = ifelse(!is.null(input[[paste0("strategyName",i)]]), input[[paste0("strategyName",i)]], paste("Strategy",LETTERS[i])))})
+      isolate({textInput(paste0("strategyName", i), paste("Strategy Name", i), value = ifelse(!is.null(input[[paste0("strategyName",i)]]), input[[paste0("strategyName",i)]], as.character(as.roman(i))))})
     })
   })
-
+  
   output$transMatrix1 <- renderUI({
     showTransMatrix(1)
-    })
+  })
   output$transMatrix2 <- renderUI({
     req(input$copyValuesParametersTM)
     showTransMatrix(input$nbStrategies)
@@ -112,41 +112,98 @@ shinyServer(function(input, output, session) {
     req(input$copyValuesParametersSP)
     showStateParam(input$nbStrategies)
   })
-  
-  output$outModel <- renderPrint({
-    ux_run_models(input = input, values = values)
+  output$costVariable <- renderUI({
+    textInput(
+      "costVariable",
+      label = "Cost Variable",
+      value = input$variableStateName1
+    )
   })
-
+  output$effectVariable <- renderUI({
+    textInput(
+      "effectVariable",
+      label = "Effect Variable",
+      value = input$variableStateName2
+    )
+  })
+  
+  
   observe({
     req(input$addParametersGP)
     isolate(values$nbGlobalParameters <- values$nbGlobalParameters + 1)
   })
-
   output$globalParameters <- renderUI({
     n <- values$nbGlobalParameters
-      req(input$nbStrategies)
-      if (input$copyValuesParametersGP[[1]] == 0) #not sure whether it's a good shiny way of doing it, but I can't figure out how to do better
-        nbStrategies <- 1
-      else 
-        nbStrategies <- input$nbStrategies
-      a <- tags$table(
-        tags$tr(
-          tags$th("Variable name"),
-          lapply(1:nbStrategies, function(x){
-            tags$th(paste("Value for strategy:"), p(input[[paste0("strategyName",x)]]))
-          })
-        ),
-        lapply(1:n, function(i){
-          tags$tr(
-            isolate(tags$td(textInput(paste0("globalParamName",i), label = NULL, value = input[[paste0("globalParamName",i)]], width="100%"))),
-            lapply(1:nbStrategies, function(x){
-              isolate(tags$td(textInput(paste0("globalParamValue",x,i), label = NULL, value = input[[paste0("globalParamValue",1,i)]], width="100%")))
-            }))
-    
+    req(input$nbStrategies)
+    if (input$copyValuesParametersGP[[1]] == 0) #not sure whether it's a good shiny way of doing it, but I can't figure out how to do better
+      nbStrategies <- 1
+    else 
+      nbStrategies <- input$nbStrategies
+    a <- tags$table(
+      tags$tr(
+        tags$th("Variable name"),
+        lapply(1:nbStrategies, function(x){
+          tags$th(paste("Value for", input[[paste0("strategyName",x)]]))
         })
-      )
+      ),
+      lapply(1:n, function(i){
+        tags$tr(
+          isolate(tags$td(textInput(paste0("globalParamName",i), label = NULL, value = input[[paste0("globalParamName",i)]], width="100%"))),
+          lapply(1:nbStrategies, function(x){
+            isolate(tags$td(textInput(paste0("globalParamValue",x,i), label = NULL, value = input[[paste0("globalParamValue",1,i)]], width="100%")))
+          }))
+        
+      })
+    )
     #}
     tagList(a,actionButton("addParametersGP", "Add a new variable"))
-})
+  })
+  
+  output$outModel <- renderUI({
+    values$model <- ux_run_models(input = input, values = values)
+    values$summary_model <- summary(values$model)
+    if (is.null(values$model)) {
+      tagList(tags$h3("Model specification incomplete"))
+    } else {
+      tagList(
+        tags$h1("Model results"),
+        tags$h3("Total values")
+      )
+    }
+  })
+  
+  output$tableResults <- DT::renderDataTable({
+    req(values$model)
+    req(values$summary_model$res)
+    DT::datatable(
+      values$summary_model$res,
+      options = list(
+        searching = FALSE,
+        paging = FALSE,
+        info = FALSE
+      )
+    )
+  })
+  output$titleICER <- renderUI({
+    req(values$model)
+    req(values$summary_model$res_comp)
+    tagList(
+      tags$h3("Efficiency frontier"),
+      tags$p(paste(values$summary_model$frontier, collapse = " -> ")),
+      tags$h3("Model comparison")
+    )
+  })
+  output$tableICER <- DT::renderDataTable({
+    req(values$model)
+    req(values$summary_model$res_comp)
+    DT::datatable(
+      values$summary_model$res_comp,
+      options = list(
+        searching = FALSE,
+        paging = FALSE,
+        info = FALSE
+      )
+    )
+  })
 })
 
--- inst/shiny/main/ui.R
+++ inst/shiny/main/ui.R
@@ -1,6 +1,11 @@
 shinyUI(
   fluidPage(
     titlePanel("HEEMOD GUI"),
+    checkboxInput(
+      "checkShowHelp",
+      "Show help",
+      value = TRUE
+    ),
     tabsetPanel(
       
       tabPanel(
@@ -13,7 +18,7 @@ shinyUI(
               numericInput(
                 "nbStates",
                 label = "Number of States",
-                value="",
+                value = "",
                 min = 1
               )
             )
@@ -24,7 +29,7 @@ shinyUI(
               numericInput(
                 "nbStateVariables",
                 label = "Number of State Variables",
-                value="",
+                value = "",
                 min = 1
               )
             )
@@ -54,6 +59,24 @@ shinyUI(
             4, 
             uiOutput("nameStrategies")
           )
+        ),
+        conditionalPanel(
+          condition = "input.checkShowHelp == 1",
+          fluidRow(
+            column(
+              4,
+              em("Number of distinct states in the model, and their names.")
+            ),
+            column(
+              4,
+              em("Number of values attached to states (such as cost, utility...), and their names.
+               Names should not contain spaces, start with '.', or special characters.")
+            ),
+            column(
+              4,
+              em("Number of strategies to compare.")
+            )
+          )
         )
         
       ),
@@ -63,14 +86,29 @@ shinyUI(
         uiOutput("globalParameters"),
         conditionalPanel(
           condition = "input.nbStrategies > 1",
-          column(
-            3,
-            offset=3,
-            actionButton("copyValuesParametersGP",
-                         "Copy values for other strategies"
+          fluidRow(
+            column(
+              3,
+              offset=3,
+              actionButton(
+                "copyValuesParametersGP",
+                "Copy values for other strategies"
+              )
             )
-          ),
-          fluidRow(hr())
+          )
+        ),
+        conditionalPanel(
+          condition = "input.checkShowHelp == 1",
+          fluidRow(
+            column(
+              4,
+              em("Optional parameters to be called in transition matrix or state values. 
+               The variable "),
+              strong("markov_cycle"),
+              em(" is defined inside the model and takes values 0, 1, 2... n at each cycle.
+               It can thus be used to define time-varying properties (such as age = 50 + markov_cycle).")
+            )
+          )
         )
       ), 
       
@@ -89,10 +127,27 @@ shinyUI(
                   "copyValuesParametersTM",
                   "Copy values for other strategies"
                 )
-              ),
-              fluidRow(hr())
-            ),
+              )
+            )
+          )
+        ),
+        fluidRow(
+          column(
+            12,
             uiOutput("transMatrix2")
+          )
+        ),
+        conditionalPanel(
+          condition = "input.checkShowHelp == 1",
+          fluidRow(
+            column(
+              4,
+              em("Matrix of transition probabilities between states.
+               References can be made to parameters computed in the previous tab.
+               The sum of probabilities per row must equal 1. The alias "),
+              strong("C"),
+              em(" (meaning probability complement) means 1 minus the row sum of other probabilities.")
+            )
           )
         )
       ), 
@@ -112,36 +167,69 @@ shinyUI(
                   "copyValuesParametersSP",
                   "Copy values for other strategies"
                 )
-              ),
-              fluidRow(hr())
-            ),
-            uiOutput("stateParameters2")
+              )
+            )
           )
         ),
         fluidRow(
           column(
-            6,
-            textInput(
-              "costVariable",
-              label = "Cost Variable",
-              placeholder = "Can be a variable name or an expression."
+            12,
+            uiOutput("stateParameters2")
+          )
+        ),
+        conditionalPanel(
+          condition = "input.checkShowHelp == 1",
+          fluidRow(
+            column(
+              4,
+              em("Values associated with each state for each strategy.")
+              )
             )
-          ),
-          column(
-            6,
-            textInput(
-              "effectVariable",
-              label = "Effect Variable",
-              placeholder = "Can be a variable name or an expression."
+        ),
+        conditionalPanel(
+          condition = "input.nbStates > 0 & input.nbStateVariables > 1",
+          fluidRow(
+            column(
+              6,
+              uiOutput(
+                "costVariable"
+              )
+            ),
+            column(
+              6,
+              uiOutput(
+                "effectVariable"
+              )
             )
+          )
+        ),
+        conditionalPanel(
+          condition = "input.checkShowHelp == 1 & input.nbStates > 0 & input.nbStateVariables > 1",
+          fluidRow(
+            column(
+              6,
+              em("State variable name representing "),
+              strong("cost"),
+              em(" in the model (or mathematical expression using variable names, such as (var1+var2)/2).")
+            ),
+            column(
+              6,
+              em("State variable name representing "),
+              strong("effect"),
+              em(" in the model (or mathematical expression using variable names, such as (var1+var2)/2).")
+              )
           )
         )
       ),
       tabPanel(
-        "Result",
-        verbatimTextOutput("outModel")
-
-        #downloadButton("downloadData", "Télécharger le tableau")
+        "Results",
+        uiOutput("outModel"),
+        DT::dataTableOutput("tableResults"),
+        uiOutput("titleICER"),
+        DT::dataTableOutput("tableICER")
+      ),
+      tabPanel(
+        "Plots"
       )
     )
   )
