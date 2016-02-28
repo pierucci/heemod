35a61f80d58f9b2afb757e53afc9186241def7c9
afp <pierucci@gmail.com>
2016-02-28 14:36:31 +0100

    :package:up version to 0.3.0

--- R/shiny.R
+++ R/shiny.R
@@ -5,7 +5,7 @@
 #' @return \code{NULL}
 #' @export
 #'
-run_shiny_main <- function() {
+run_shiny <- function() {
     app_dir <- system.file("shiny", "main", package = "heemod")
     if (app_dir == "") {
       stop("Could not find 'shiny' directory. Try re-installing `heemod`.", call. = FALSE)
