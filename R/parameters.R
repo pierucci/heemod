1ad38c9405b1d88d4ad99b37e4593525afc52a72
afp <pierucci@gmail.com>
2016-03-16 15:31:32 +0100

    :beetle:fix printing param when long expression

--- R/parameters.R
+++ R/parameters.R
@@ -158,8 +158,10 @@ print.uneval_parameters <- function(x, ...) {
   cat(sprintf("%i unevaluated parameter%s.\n\n",
               length(x), plur(length(x))))
   n <- names(x)
-  ex <- unlist(lapply(x, function(y) deparse(y$expr)))
-  
+  ex <- unlist(lapply(x, function(y) deparse(y$expr, width.cutoff = 500L)))
+  stopifnot(
+    length(n) == length(ex)
+  )
   cat(paste(n, ex, sep = " = "), sep = "\n")
 }
 
