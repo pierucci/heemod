d9e2f487fcd1716c488cf871e5f92969143b0e28
afp <pierucci@gmail.com>
2016-06-28 21:32:39 +0200

    :sparkles:new import tabular data functions

---
+++ R/import_tabular.R
@@ -0,0 +1,53 @@
+#' Import Analysis from Tabular Data
+#'
+#' @param path Path to a folder containing the model information.
+#'
+#' Import an analysis saved in a folder as tabular data.
+#'
+#' A saved analysis follows a very specific format. Use
+#' \code{\link{create_import_template}} or \code{\link{export}}
+#' to populate the folder.
+#'
+#' @return Same result as \code{\link{run_models}}.
+#' @export
+#'
+import <- function(path) {
+  # read file names in folder
+  # parse file names to extract
+  #   model names
+  parsed_folder <- parse_import_folder(path)
+  # check coherence
+  #   message if recycling needed
+  #   message if optional file missing
+  #   warning if file not following naming scheme
+  #   stop if crucial file missing
+  check_import_folder(parsed_folder)
+  # import files for model definition
+  list_import_models
+  # build models from files
+  list_models <- lapply(
+    list_import_models,
+    create_model
+  )
+  # import options for running models
+  list_import_option
+  # run models from files
+  build_run_models
+}
+
+#' Create a Tabular Data Template
+#'
+#' @param path Path to an empty folder where
+#'  the template will be created.
+#' @param model_names Names of models.
+#' @param state_names Names of states.
+#' @param state_value_names Names of state values.
+#'
+#' @return \code{NULL}
+#' @export
+#'
+create_import_template <- function(path, model_names,
+                                   state_names,
+                                   state_value_names) {
+
+}
