76cddb3feec2d1e85773fd862db5f8d13fe4e74c
afp <pierucci@gmail.com>
2016-08-30 03:25:57 +0200

    :beetle:remove forgotten browser

--- R/model_print.R
+++ R/model_print.R
@@ -101,7 +101,7 @@ plot.run_models <- function(x, type = c("counts", "ce"),
       tab_counts <- do.call("rbind", tab_counts_pieces)
       tab_counts$.model <- factor(tab_counts$.model, levels = names(model_info))
       pos_cycle <- pretty(seq_len(nrow(tab_counts)), n = min(nrow(tab_counts), 10))
-      browser()
+      
       tab_counts <- tidyr::gather_(
         data = tab_counts,
         key_col = "key",
