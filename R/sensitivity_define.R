2ce82d78954213e5718516b4f7a94c4c413ee6c4
afp <pierucci@gmail.com>
2016-09-16 11:02:50 +0200

    :wrench:rewrite define_sens #123

--- R/sensitivity_define.R
+++ R/sensitivity_define.R
@@ -21,26 +21,41 @@
 #' )
 #' 
 define_sensitivity <- function(...) {
-  .dots <- list(...)
+  .dots <- lazyeval::lazy_dots(...)
   
   if (! length(.dots) %% 3 == 0) {
     stop("Incorrect number of elements in correlation definition, the correct form is A, B, cor(A, B)...")
   }
   
-  function(i) {
-    if (i %% 3 == 0) {
-      lazyeval::lazy_eval(.dots[[i]])
+  par_names <- character()
+  min_dots <- lazyeval::lazy_dots()
+  max_dots <- lazyeval::lazy_dots()
+  
+  for (i in seq_along(.dots)) {
+    if (i %% 3 == 1) {
+      par_names <- c(par_names, deparse(.dots[[i]]$expr))
+    } else if (i %% 3 == 2) {
+      min_dots <- c(min_dots, .dots[[i]])
     } else {
-      deparse(.dots[[i]]$expr)
+      max_dots <- c(max_dots, .dots[[i]])
     }
   }
+  
+  names(min_dots) <- par_names
+  names(max_dots) <- par_names
+  
   define_sensitivity_(par_names, min_dots, max_dots)
 }
 
 #' @rdname define_sensitivity
 define_sensitivity_ <- function(par_names, min_dots, max_dots) {
   
-  check_names(names(.dots))
+  check_names(par_names)
+  
+  stopifnot(
+    all(par_names == names(min_dots)),
+    all(par_names == names(max_dots))
+  )
   
   if (! all(unlist(lapply(.dots, function(x) length(x))) == 2)) {
     stop("Incorrect number of elements in sensitivity definition, the correct form is A = c(A_min, A_max)...")
