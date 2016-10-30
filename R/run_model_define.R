3ab9e91c5a1aa4b64f94ed9853d6a45626c66a32
afp <pierucci@gmail.com>
2016-10-30 20:11:29 +0100

    :wrench:delete base model

--- R/run_model_define.R
+++ R/run_model_define.R
@@ -44,8 +44,6 @@
 #'   cost-effectiveness plane.
 #' @param effect Names or expression to compute effect on 
 #'   the cost-effectiveness plane.
-#' @param base_model Name of base model used as reference. 
-#'   By default the model with the lowest effectiveness.
 #' @param method Counting method.
 #' @param list_models List of models, only used by 
 #'   \code{run_model_} to avoid using \code{...}.
@@ -64,7 +62,6 @@ run_model <- function(...,
                        method = c("life-table", "beginning", "end",
                                   "half-cycle"),
                        cost = NULL, effect = NULL,
-                       base_model = NULL,
                        state_cycle_limit = NULL) {
   
   list_models <- list(...)
@@ -79,7 +76,6 @@ run_model <- function(...,
     method = method,
     cost = lazyeval::lazy_(substitute(cost), env = parent.frame()),
     effect = lazyeval::lazy_(substitute(effect), env = parent.frame()),
-    base_model = base_model,
     state_cycle_limit = state_cycle_limit
   )
 }
@@ -91,7 +87,7 @@ run_model_ <- function(list_models,
                         init,
                         cycles,
                         method,
-                        cost, effect, base_model,
+                        cost, effect,
                         state_cycle_limit) {
   
   if (! is.wholenumber(cycles)) {
@@ -195,9 +191,7 @@ run_model_ <- function(list_models,
   res <- Reduce(dplyr::bind_rows, list_res) %>% 
     dplyr::mutate_(.dots = ce)
   
-  if (is.null(base_model)) {
-    base_model <- get_base_model(res)
-  }
+  base_model <- get_base_model(res)
   
   structure(
     res,
