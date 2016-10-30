5d03ed36f6f21f6b52a2d93af892071983e43224
afp <pierucci@gmail.com>
2016-10-30 20:33:59 +0100

    :beetle:hopefully correct implementation of efficiency frontier

--- R/efficiency_frontier.R
+++ R/efficiency_frontier.R
@@ -9,15 +9,25 @@
 get_frontier <- function(x) {
   base_model <- get_base_model(x)
   
-  # remove models less effective than base
-  tab_model <- x[x$.effect >= x$.effect[x$.model_names == base_model], ]
+  # recursive function
+  # gets strategy with lowest icer -> next on frontier
+  # filters strat less effective than next on frontier
+  # re-apply function on remaining
+  f <- function(x) {
+    if (nrow(x) == 0) {
+      NULL
+    } else {
+      x <- dplyr::mutate_(x, .icer = ~ .cost / .effect) %>% 
+        dplyr::arrange_(.dots = list(~.icer, ~ .effect))
+      
+      res <- (dplyr::slice(x, 1))$.model_names
+      effect_res <- x$.effect[x$.model_names == res]
+      x_res <- x %>% dplyr::filter_(~ .effect > effect_res)
+      
+      c(res, f(x_res))
+    }
+  }
   
-  tab_icer <- compute_icer(tab_model)
-  
-  tab_icer <- tab_icer[order(tab_icer$.icer, - tab_icer$.effect), ]
-  
-  # higher ICER must be for better effect
-  tab_icer <- tab_icer[tab_icer$.effect >= cummax(tab_icer$.effect), ]
-  
-  tab_icer$.model_names[order(tab_icer$.effect)]
+  c(base_model,
+    f(x %>% dplyr::filter_(~ .model_names != base_model)))
 }
