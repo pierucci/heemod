723ff1020ce674fcb13e3cd053f978bbe6e5b9c3
Matt Wiener <AA0113@merck.com>
2017-02-15 12:00:17 +0100

    update some example files, partly to match distribution names

--- tests/testthat/test_tabular_input.R
+++ tests/testthat/test_tabular_input.R
@@ -363,7 +363,10 @@ test_that(
     )
     
     expect_error(
-      create_model_from_tabular(states1, NULL, NULL, new.env())
+      create_model_from_tabular(states1, NULL, NULL, new.env()),
+      "either a transition matrix (tm_info) or a partitioned
+         survival model (surv_info) must be defined",
+      fixed = TRUE
     )
   }
 )
@@ -682,6 +685,44 @@ test_that(
         "psa","demographics")
     )
     
+    ## make sure structure of dsa and psa objects haven't 
+    ## changed too much (so we don't get problems with output)
+    expect_identical(
+      names(result$dsa),
+            c("dsa", "variables", "model")
+    )
+    expect_identical(
+      class(result$dsa$dsa),
+      c("tbl_df", "tbl", "data.frame")
+    )
+    
+    expect_identical(
+      names(result$psa),
+      c("psa", "run_model", "model", "N", "resamp_par")
+    )
+    expect_identical(
+      class(result$psa$psa),
+      c("tbl_df", "tbl", "data.frame")
+    )
+    
+    expect_identical(
+      names(result$demographics),
+      c("updated_model", "newdata", "model",
+        "combined_model", "has_weights", "weights")
+    )
+    expect_identical(
+      class(result$demographics$updated_model),
+      c("tbl_df", "tbl", "data.frame")
+    )
+    expect_equal(
+      round(as.numeric(result$dsa$dsa[1,1:2]), 2),
+      c(201310.42, 14660.95)
+    )
+    expect_equal(
+      as.numeric(result$demographics$updated_model[1, 1:3]),
+      c(46809.972, 4533.945, 82)
+    )
+    
     expect_output(
       print(result$model_runs),
       "-223.3065   0.04426563 -5044.693",
@@ -707,6 +748,29 @@ test_that(
     
   }
 )
+
+  test_that("running defined by strategies gets same results",
+            {
+              result <- run_model_tabular(
+                location = system.file("tabular/thr", package = "heemod"),
+                save = FALSE, overwrite = FALSE, run_psa = FALSE
+              )
+              result_strategy <- run_model_tabular(
+                location = system.file("tabular/thr", package = "heemod"),
+                "REFERENCE_strategy.csv",
+                save = FALSE, overwrite = FALSE, run_psa = FALSE
+              )
+              
+              expect_identical(get_counts(result$model_runs, "new"),
+                               get_counts(result_strategy$model_runs, "new")
+              )
+              expect_identical(result$demographics$updated_model,
+                               result_strategy$demographics$updated_model
+              )
+                           
+            }
+  )
+
 
 test_that(
   "safe conversion works", {
