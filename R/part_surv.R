69fb84d8cfbab7dd302879f85c7d7b2d1d9eb862
Wiener, Matthew <Matthew_Wiener@merck.com>
2017-03-02 13:45:38 +0100

    fix the case when state names are provided for partial survival objects, but names(state_names) is NULL. Add corresponding tests.

--- R/part_surv.R
+++ R/part_surv.R
@@ -64,8 +64,12 @@ define_part_surv <- function(pfs, os, state_names,
         "death"
       )
     }
-  } else if (terminal_state) {
-    warning("Argument 'terminal_state' ignored when state names are given.")
+  } 
+  if(is.null(names(state_names))) {
+    if (terminal_state) {
+      warning("Argument 'terminal_state' ignored when state names are given.")
+    }
+    state_names <- fix_part_surv_state_names(state_names)
   }
   
   define_part_surv_(
@@ -73,7 +77,8 @@ define_part_surv <- function(pfs, os, state_names,
     os = lazyeval::lazy_(substitute(os), env = parent.frame()),
     state_names = state_names,
     cycle_length = cycle_length)
-}
+  }
+
 
 #' @export
 #' @rdname define_part_surv
@@ -197,3 +202,49 @@ compute_counts.eval_part_surv <- function(x, init,
   
   structure(res, class = c("cycle_counts", class(res)))
 }
+
+fix_part_surv_state_names <-
+  function(state_names) {
+    death_state <- c(
+      grep("death", state_names, ignore.case = TRUE),
+      grep("dead", state_names, ignore.case = TRUE)
+    )
+    progfree_state <- grep("free", state_names, ignore.case = TRUE)
+    progressive_state <-
+      setdiff(grep("progress", state_names, ignore.case = TRUE),
+              progfree_state)
+    terminal_state <- grep("terminal", state_names, ignore.case = TRUE)
+    
+    if (length(death_state) != 1)
+      stop("state representing death must be named ",
+           "'death' or 'dead' (case insensitive)")
+    if (length(progfree_state) != 1)
+      stop("progression free state (only) must have 'free' in its name")
+    if (length(progressive_state) != 1)
+      stop("progression free state must have 'progress' ",
+           "but not 'free', in its name")
+    
+    stopifnot(length(state_names) %in% c(3, 4))
+    if (length(state_names) == 3)
+      names(state_names) <-
+      c("progression_free",
+        "progression",
+        "death")[c(progfree_state, progressive_state,
+                   death_state)]
+    if (length(state_names) == 4) {
+      if (length(terminal_state) == 0)
+        stop(
+          "if there are four states, you must have a state called 'terminal'",
+          "(not case sensitive)"
+        )
+      names(state_names) <-
+        c("progression_free",
+          "progression",
+          "terminal",
+          "death")[c(progfree_state,
+                     progressive_state,
+                     terminal_state,
+                     death_state)]
+    }
+    state_names
+  }
--- tests/testthat/test_part_surv.R
+++ tests/testthat/test_part_surv.R
@@ -18,17 +18,21 @@ surv_dist4 <- define_survival(
 )
 
 
+suppressMessages(
 ps <- define_part_surv(
   pfs = project(surv_dist3, surv_dist4, at=365),
   os = project(surv_dist1, surv_dist2, at=365),
   cycle_length = c(365, 365)
 )
-
+)
+suppressMessages(
 ps1 <- define_part_surv(
   pfs = project(surv_dist3, surv_dist4, at=365) %>% apply_hr(0.8),
   os = project(surv_dist1, surv_dist2, at=365) %>% apply_hr(0.8),
   cycle_length = c(365, 365)
 )
+)
+
 sA <-  define_state(
   cost = 10000, ut = 1
 )
@@ -56,3 +60,60 @@ resPS <- run_model(
   effect = ut,
   method = "end"
 )
+
+ps <- define_part_surv(
+  pfs = project(surv_dist3),
+  os = project(surv_dist1),
+  state_names = c("ProgressionFree", "Progressive", "Death"),
+  cycle_length = c(365, 365)
+)
+
+test_that("errors with inappropriate state names",
+          {
+            expect_error(
+              ps <- define_part_surv(
+                pfs = project(surv_dist3),
+                os = project(surv_dist1),
+                state_names = c("NoDisease", "Progressive", "Death"),
+                cycle_length = c(365, 365)
+              ),
+              "progression free state (only) must have 'free' in its name",
+              fixed = TRUE
+            )
+            expect_error(
+              ps <- define_part_surv(
+                pfs = project(surv_dist3),
+                os = project(surv_dist1),
+                state_names = c("ProgressionFree", "Progressive", "Kaput"),
+                cycle_length = c(365, 365)
+              ),
+              "state representing death",
+              fixed = TRUE
+            )
+            expect_error(
+              ps <- define_part_surv(
+                pfs = project(surv_dist3),
+                os = project(surv_dist1),
+                state_names = c("ProgressionFree", "Progressive",
+                                "uh-oh", "Death"),
+                cycle_length = c(365, 365)
+              ),
+              "if there are four states, you must have a state called 'terminal'",
+              fixed = TRUE
+            )
+            expect_error(
+              ps <- define_part_surv(
+                pfs = project(surv_dist3),
+                os = project(surv_dist1),
+                state_names = c(
+                  "ProgressionFree",
+                  "Progressivebutfree",
+                  "terminal",
+                  "Death"
+                ),
+                cycle_length = c(365, 365)
+              ),
+              "progression free state (only) must have 'free' in its name",
+              fixed = TRUE
+            )
+          })
