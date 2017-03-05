08871dcfff04eb6d6b429d2bbc0ced6f56222e11
Antoine Filipovic-Pierucci <pierucci@gmail.com>
2017-03-05 14:09:27 +0100

    :sparkles:custom correction method & part surv correction

---
+++ R/correct_counts.R
@@ -0,0 +1,31 @@
+correct_counts <- function(x,
+                           method = c("life-table", "beginning",
+                                      "end")) {
+  
+  if (! is.function(method)) {
+    method <- match.arg(method)
+    
+    n0 <- x[- nrow(x), ]
+    n1 <- x[-1, ]
+    
+    switch(
+      method,
+      "beginning" = {
+        out <- n1
+      },
+      "end" = {
+        out <- n0
+      },
+      "life-table" = {
+        out <- (n0 + n1) / 2
+      })
+  } else {
+    out <- method(x)
+  }
+  
+  if (nrow(out) != nrow(x) - 1) {
+    stop("State membership correction applied to an n-row table should return a table with n-1 rows.")
+  }
+  
+  return(out)
+}
