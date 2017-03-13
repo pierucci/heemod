cfc26cb059eb6c5eb0698599977042163d2acd62
Wiener, Matthew <Matthew_Wiener@merck.com>
2017-03-13 03:29:06 +0100

    fix things up so we can use syntax like "fit_matrix('exp')" and it gets converted to something that can be evaluated.

--- R/construct_survival.R
+++ R/construct_survival.R
@@ -6,7 +6,6 @@ construct_survival <-
     ##   for example:  fit_matrix[["exp"]]
     ##                 exp(rate = 0.5)
     
-    surv_def <- tibble::as_tibble(surv_def)
     
     
     strategies <- unique(surv_def$.strategy)
@@ -14,14 +13,35 @@ construct_survival <-
     
     ## evaluate distribution text to get distributions
     
+    ## if we have something of the form fit_matrix("exp") on a line
+    ##   with .strategy A and .type os, replace it with
+    ##   fit_matrix[["exp", "A"]]$os.
     
-    
+    surv_def$dist <- 
+      lapply(seq(along = surv_def$dist), function(i){
+        is_define_dist <- 
+          length(grep("define_survival", surv_def[i, "dist"])) > 0
+        if(is_define_dist)
+          res <- surv_def[i, "dist"]
+        else{
+        this_dist <- as.character(surv_def[i, "dist"])
+        this_strategy <- as.character(surv_def[i, ".strategy"][[1]])
+        this_type <- as.character(surv_def[i, ".type"][[1]])
+        res <- 
+          gsub("()\\((.*)\\)", "\\1[[\\2, zrw1]]\\$zrw2", this_dist)
+        res <- gsub("zrw1", paste("'", this_strategy, "'", sep = ""), res)
+        res <- gsub("zrw2", this_type, res)
+        }
+      res
+      })
+  
     surv_def$dist <- 
       lapply(surv_def$dist, function(this_dist){
         lazyeval::lazy_(this_dist, 
                         env = parent.frame())
       })
-        
+    surv_def <- tibble::as_tibble(surv_def)
+    
     res1 <-
       lapply(strategies,
              function(this_strategy) {
